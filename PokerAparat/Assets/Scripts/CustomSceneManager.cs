5744245aa3005f1398921bb019ed73f7ace3d43c
Adam M <amehtic@gmail.com>
2021-11-19 18:31:05 +0100

    logo, firebase initialization

---
+++ PokerAparat/Assets/Scripts/CustomSceneManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class CustomSceneManager
+{
+    public static void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public static void RestartCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public static AsyncOperation LoadSceneAsync(string sceneName)
+    {
+        return SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public static Scene CurrentScene()
+    {
+        return SceneManager.GetActiveScene();
+    }
+
+    public static string GetSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+}
---
+++ PokerAparat/Assets/Scripts/LogoScreen/LogoScreen.cs
@@ -0,0 +1,71 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoScreen : MonoBehaviour
+{
+    public Canvas SplashScreenCanvas;
+    private CanvasGroup splashAlpha;
+    private Image logoImage;
+    public List<Sprite> LogosToShow;
+
+    void Awake()
+    {
+        if (SplashScreenCanvas != null)
+        {
+            logoImage = SplashScreenCanvas.GetComponentInChildren<Image>();
+            splashAlpha = SplashScreenCanvas.GetComponent<CanvasGroup>();
+            FadeIn();
+        }
+    }
+
+    void Start()
+    {
+        //Invoke("FadeOutMethod", LoadTime);
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        AsyncOperation async = null;
+
+        async = CustomSceneManager.LoadSceneAsync("game");
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    void FadeIn()
+    {
+        if (LogosToShow != null && LogosToShow.Count > 0)
+        {
+            splashAlpha.alpha = 0f;
+            Sprite _sprite = LogosToShow[0];
+            if (_sprite != null)
+            {
+                LogosToShow.RemoveAt(0);
+                logoImage.sprite = _sprite;
+            }
+
+            splashAlpha.DOFade(1f, 0.5f).SetDelay(.5f).OnComplete(() =>
+            {
+                FadeOut();
+            }).Play();
+        }
+        else
+        {
+            StartCoroutine(LoadNewScene());
+        }
+    }
+
+    void FadeOut()
+    {
+        splashAlpha.DOFade(0f, 0.5f).OnComplete(() =>
+        {
+            FadeIn();
+        }).SetDelay(0.5f).Play();
+    }
+}
